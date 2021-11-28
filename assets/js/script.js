0edd05814fb9fecd8e03c9f35023b8acfacd4e5e
cnfionawu <76815461+cnfionawu@users.noreply.github.com>
2021-11-27 21:53:30 -0800

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMode() {
-    x = document.querySelector('body');
-    x.classList.toggle('white-mode');
-    
-}
-
-let button = document.getElementById('abox');
-button.onclick = toggleMode;
